d67178bc189451abe92cb2fd8a67bd8ae78ebdae
朱雀　翔 <KTC25A31E0009@edu.kyoto-tech.ac.jp>
2026-08-11 03:03:54 +0900

    check

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        video: resolve(__dirname, 'video.html'),
+      },
+    },
+  },
 });
